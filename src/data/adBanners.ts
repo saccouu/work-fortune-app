9b44246a9a06b76181274bdbcd74d86419697d07
saccouu <yskwskk@yahoo.co.jp>
2026-07-06 11:13:32 +0900

    Update adBanners.ts

--- src/data/adBanners.ts
+++ src/data/adBanners.ts
@@ -6,10 +6,11 @@
 // （見えない計測用の画像タグが含まれていることがありますが、絶対に消さないでください）
 //
 // label: バナーの左上に小さく表示される見出し文字（例: "あなたにおすすめ"）
-
 export const AD_BANNERS = [
   {
-    htmlCode: `<span class="a8ad 4eqbZ75-g7-voylbN8"></span><script type="text/javascript" src="//statics.a8.net/ad/ad.js"></script><script type="text/javascript">a8adscript('body').showAd({"req": {"mat":"3Z4WIB+45GOHE+2PEO+HUSFL","alt":"商品リンク","id":"4eqbZ75-g7-voylbN8"},"goods": {"ejp":"h"+"ttps://coconala.com/categories/3?service_kind=1","imu":"h"+"ttps://www.bbauranai.jp/ads/banner1.png"}});</script>`,
+    htmlCode: `<a href="https://px.a8.net/svt/ejp?a8mat=3Z4WIB+45GOHE+2PEO+HUSFL&a8ejpredirect=https%3A%2F%2Fcoconala.com%2Fcategories%2F3%3Fservice_kind%3D1" rel="nofollow">
+<img border="0" alt="" src="https://www.bbauranai.jp/ads/banner1.png"></a>
+<img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3Z4WIB+45GOHE+2PEO+HUSFL" alt="">`,
     label: 'あなたにおすすめ',
   },
   {
