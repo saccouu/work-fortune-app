b9132a6abd75715a07f6e0f7e70c8400aefd9fc5
saccouu <yskwskk@yahoo.co.jp>
2026-07-20 16:41:30 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'あなたに向いている仕事＆資格診断',
+  title: 'あなたにピッタリの資格診断',
   description: '11の質問で分かる、AI時代でも必要とされるあなたの資質を診断します',
 }
 
