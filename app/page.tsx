39240e0fa3aa97f8681f43bce12da7c3f5e5915b
saccouu <yskwskk@yahoo.co.jp>
2026-07-21 13:02:15 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -329,7 +329,7 @@ export default function Home() {
             <img
               src="/ads/result.png"
               alt="診断結果"
-              className="w-84 mx-auto mb-2"
+              className="w-84 mx-auto -mb-2"
             />
           <div className="bg-white p-6 rounded-3xl border-2 border-[#3D3226] shadow-[4px_4px_0_0_#3D3226] text-left">
             {result.reason.map((line, idx) => (
